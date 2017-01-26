b62668fc9a25ca286878d7391b43882deee3af0c
ellert90 <yura.igm@gmail.com>
2017-01-26 15:03:04 +0200

    finished js34

--- js34/script.js
+++ js34/script.js
@@ -1,108 +1,88 @@
+
 var ques = {
-	topic: 'Programing test',
+  topic: 'Programing test',
 
-	q0: {
-		ask: 'Question №1',
-		ans1: 'Answer A',
-		ans2: 'Answer B',
-		ans3: 'Answer C',
-		tru_ans: 'Answer B'
-	},
+  q0: {
+    ask: 'Question №1',
+    ans1: 'Answer 1-A',
+    ans2: 'Answer 1-B',
+    ans3: 'Answer 1-C',
+    tru_ans: 'Answer B'
+  },
 
-	q1: {
-		ask: 'Question №2',
-		ans1: 'Answer A',
-		ans2: 'Answer B',
-		ans3: 'Answer C',
-		tru_ans: 'Answer C'
-	},
+  q1: {
+    ask: 'Question №2',
+    ans1: 'Answer 2-A',
+    ans2: 'Answer 2-B',
+    ans3: 'Answer 2-C',
+    tru_ans: 'Answer C'
+  },
 
-	q2: {
-		ask: 'Question №3',
-		ans1: 'Answer A',
-		ans2: 'Answer B',
-		ans3: 'Answer C',
-		tru_ans: 'Answer A'
-	},
+  q2: {
+    ask: 'Question №3',
+    ans1: 'Answer 3-A',
+    ans2: 'Answer 3-B',
+    ans3: 'Answer 3-C',
+    tru_ans: 'Answer A'
+  },
 
-	button: 'Check result'
-}
-function addObj(obj) {
-  var topic = ques.topic,
-      ask0 = ques.q0.ask,
-			ask1 = ques.q1.ask,
-			ask2 = ques.q2.ask,
-			button = ques.button;
-      var arr0 = [ques.q0.ans1, ques.q0.ans2, ques.q0.ans3];
-      var arr1 = [ques.q1.ans1, ques.q1.ans2, ques.q1.ans3];
-      var arr2 = [ques.q2.ans1, ques.q2.ans2, ques.q2.ans3];
+  button: 'Check result'
+};
 
-			function generateNodes() {
+var ask   = [ques.q0.ask, ques.q1.ask, ques.q2.ask];
+var arr0  = [ques.q0.ans1, ques.q0.ans2, ques.q0.ans3];
+var arr1  = [ques.q1.ans1, ques.q1.ans2, ques.q1.ans3];
+var arr2  = [ques.q2.ans1, ques.q2.ans2, ques.q2.ans3];
 
-		        function addTextNode(el, text) {
-		            var elem = document.createElement(el);
-		            var newtext = document.createTextNode(text);
-		            elem.appendChild(newtext);
-		            return elem;
-		        }
 
-		        function addClass(el, name) {
-		          var elem = document.createElement(el);
-		          elem.setAttribute("class", name);
-		          return elem;
-		        }
+var body  = document.body;
+var div   = document.createElement('div');                 		   
+var h1    = document.createElement('h1');                          
+var ol    = document.createElement('ol');						   
+var list0 = document.createElement('li');	
+var list1 = document.createElement('li');
+var list2 = document.createElement('li');					  									   
+var btn   = document.createElement('button');					   
+var label = document.createElement('label');	
+var input = document.createElement('input');
 
-		        function addTextNodeClass(el, name, text) {
-		            var elem = document.createElement(el);
-		            elem.setAttribute("class", name);
-		            var newtext = document.createTextNode(text);
-		            elem.appendChild(newtext);
-		            return elem;
-		        }
+div.setAttribute('class', 'wrapper');							 
 
-		        function addLi(arr, el) {
-		          for (var i = 0; leng = arr.length, i < leng; i++) {
-		            el.appendChild(addTextNode("li", arr[i]));
-		          }
-		          return this;
-		        }
+function addLi(arr, el, par) {
+          for (var i = 0; leng = arr.length, i < leng; i++) {
+          	(function() {
+          		val = 0;
+	          	var el = document.createElement('label');
+	          	var input = document.createElement('input');
+	          	input.setAttribute('type', 'checkbox');
+	          	input.setAttribute('value', 'val' + val++);
+	          	el.appendChild(input);
+	          	el.appendChild(document.createTextNode(arr[i]));
+	          	par.appendChild(el);
+	          	ol.appendChild(par);
+	          	console.log(el);
+          	})();
+          }
+          return this;
+        }
 
-    var wrp = addClass("div", "wrapper");
-    var hed = addClass("h1", "head");
-    var ol = addClass("ol", "Numbered_li");
-		var li = addClass("li", "Numbered_li");
-    var ul = addClass("ul", "Unnumvered_li");
-		var ul1 = addClass("ul", "Unnumvered_li");
-		var ul2 = addClass("ul", "Unnumvered_li");
-		var list0 = addClass("li", "menu_item");
-		var list1 = addClass("li", "menu_item");
-		var list2 = addClass("li", "menu_item");
-		var btn = addClass("button", "send");
 
-    addLi(arr0, list0);
-    addLi(arr1, list1);
-		addLi(arr2, list2);
+h1.appendChild(document.createTextNode(ques.topic));
+list0.appendChild(document.createTextNode(ques.q0.ask));
+list1.appendChild(document.createTextNode(ques.q1.ask));
+list2.appendChild(document.createTextNode(ques.q2.ask));	
+btn.appendChild(document.createTextNode(ques.button));	
+input.setAttribute('type', 'checkbox');
+ 
+addLi(arr0, label, list0);	
+addLi(arr1, label, list1);
+addLi(arr2, label, list2);
 
-		wrp.appendChild(addTextNode("h1", topic));
-		ul.appendChild(list0);
-		ul.appendChild(list1);
-		ul.appendChild(list2);
-		li.appendChild(ul);
-		ol.appendChild(addTextNode("li", ask0));
-		ol.appendChild(addTextNode("li", ask1));
-		ol.appendChild(addTextNode("li", ask2));
-		wrp.appendChild(ol);
-		wrp.appendChild(addTextNode("button", button));
+div.appendChild(h1);  	
+div.appendChild(ol);		
+div.appendChild(btn);	
+ol.appendChild(list0);
 
-		return wrp;
-    }
 
-    return generateNodes();
-}
 
-var doc = document.body;
-
-var frag = document.createDocumentFragment();
-frag.appendChild(addObj(ques).cloneNode(true));
-var myClone = frag.cloneNode(true);
-doc.appendChild(myClone);
+body.appendChild(div);	
