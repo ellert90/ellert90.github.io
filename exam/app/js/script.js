c0f951996836f9db5b1643385438884db08f0094
ellert90 <yura.igm@gmail.com>
2017-05-20 16:35:52 +0300

    fixed js

--- exam/app/js/script.js
+++ exam/app/js/script.js
@@ -1,4 +1,59 @@
-"use strict";$(function(){function e(){$("#result").empty(),$(".img__container").empty(),t=$("#search_in").val();var e="https://pixabay.com/api/?key=4828969-66d42077835bb109c3bb61bef&q="+t+"&image_type=photo&pretty=true";$.ajax({url:e,success:function(e){console.log(e);for(var t=0;t<7;t++){var o='<div class="img__container"><div style=" background-image: url('+e.hits[t].webformatURL+'); background-size: cover; background-position: center;" class="img__result"></div><span class="img__text">'+e.hits[t].tags+"</span></div>";$("#result").append(o)}},error:function(){console.log(error)}})}e();var t;$("#search_in").keyup(function(){13==event.keyCode&&e()}),$("#button").on("click",function(t){t.preventDefault(),e()}),$(window).resize(function(){var e=$("#result");e.imagesLoaded(function(){e.masonry({itemSelector:".img__container",percentPosition:!0,gutter:10})}),$(document).width()<768?$("#result").css("min-height","910px"):$("#result").css("min-height","990px")}),console.log($(document).width()),setTimeout(function(){$(document).width()<768?$("#result").css("height","910px"):$("#result").css("height","970px"),console.log("time")},1e3),$(".slider__hider").jcarousel(),$(".slider__left").on("jcarouselcontrol:active",function(){$(this).removeClass("inactive")}).on("jcarouselcontrol:inactive",function(){$(this).addClass("inactive")}).jcarouselControl({target:"-=1"}),$(".slider__right").on("jcarouselcontrol:active",function(){$(this).removeClass("inactive")}).on("jcarouselcontrol:inactive",function(){$(this).addClass("inactive")}).jcarouselControl({target:"+=1"})});r: 10
+"use strict";
+
+$(function() {
+    action();
+    //BUTTON
+    var searchValue;
+    $('#search_in').keyup(function(){
+        if(event.keyCode==13)
+           {
+              //  $('#result').empty();
+              action();
+           };
+    });
+
+    $('#button').on('click', function(e) {
+      e.preventDefault();
+      action();
+
+
+    });
+    function action(){
+
+      $('#result').empty();
+      $('.img__container').empty();
+
+      searchValue = $('#search_in').val();
+
+      var urla = 'https://pixabay.com/api/?key=4828969-66d42077835bb109c3bb61bef&q=' + searchValue + '&image_type=photo&pretty=true';
+
+
+      $.ajax({
+        url: urla,
+        success: function(data) {
+          console.log(data);
+          for (var i = 0; i < 7; i++) {
+            var $img = ('<div class="img__container"><div style=" background-image: url('+data.hits[i].webformatURL+'); background-size: cover; background-position: center;" class="img__result"></div><span class="img__text">' + data.hits[i].tags + '</span></div>');
+            $('#result').append($img);
+          }
+        },
+        error: function() {
+          console.log(error);
+        }
+      });
+    };
+
+
+
+$(window).resize(function(){
+  var $container = $('#result');
+                 $container.imagesLoaded( function() {
+                 $container.masonry({
+                  // options
+                     itemSelector: '.img__container',
+                    //  columnWidth: '.persent-size',
+                     percentPosition: true,
+                     gutter: 10
                      });
                   });
 
