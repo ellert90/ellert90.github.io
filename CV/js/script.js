f000560662032eca595de0a2f1fb08bf90c4e988
ellert90 <yura.igm@gmail.com>
2017-05-22 14:16:54 +0300

    cv

---
+++ CV/js/script.js
@@ -0,0 +1,13 @@
+// "use strict";
+
+// var css_block = document.getElementsByClassName('css');
+// console.log(css_block);
+
+function load() {
+  ph.style.margin='30px auto';
+  css.style.margin='15px auto';
+}
+
+// ph.onclick = load();
+
+window.onload = load();
