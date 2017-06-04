a4e8ce9cb451b848a0042a3dd54e7896ca44b361
ellert90 <yura.igm@gmail.com>
2017-06-04 20:38:51 +0300

    js19-20

--- js19-20/dist/js/src/script.js
+++ js19-20/dist/js/src/script.js
@@ -1,87 +1,22 @@
-"use strict";
-
-$(function() {
-    action();
-    //BUTTON
-    var searchValue;
-    $('#search_in').keyup(function(){
-        if(event.keyCode==13)
-           {
-              action();
-           }
-    })
-
-    $('#button').on('click', function(e) {
-      e.preventDefault();
-      action();
-
-
-    });
-    function action(){
-
-      $('#result').empty();
-      $('.img__container').empty();
-
-      searchValue = $('#search_in').val();
-
-      var urla = 'https://pixabay.com/api/?key=4828969-66d42077835bb109c3bb61bef&q=' + searchValue + '&image_type=photo&pretty=true';
-
-
-      $.ajax({
-
-        url: urla,
-
-        success: function(data) {
-
-          for (var i = 0; i < 7; i++) {
-            var $img = ('<div class="img__container"><div style="background: url('+data.hits[i].webformatURL+'); background-size: cover; background-position: center;" class="img__search"></div><span class="img__text">' + data.hits[i].tags + '</span></div>');
-            $('#result').css("height", "100%").append($img);
-          }
-        },
-        error: function() {
-          console.log('error fetching!');
-        }
-      });
-
-    };
-});
-
-// $('#result').masonry({
-//   itemSelector: '.img__container',
-//   gutter: 30
-// });
-var $container = $('#result');
-               $container.imagesLoaded( function() {
-               $container.masonry({
-                // options
-                   itemSelector: '.img__container',
-                  //  columnWidth: '.persent-size',
-                   percentPosition: true,
-                   gutter: 10
-                   });
-                });
-  //slider
-
-  $('.works__hider').jcarousel();
-
-        $('.slider-left')
-            .on('jcarouselcontrol:active', function() {
-                $(this).removeClass('inactive');
-            })
-            .on('jcarouselcontrol:inactive', function() {
-                $(this).addClass('inactive');
-            })
-            .jcarouselControl({
-                target: '-=1'
-            });
-
-        $('.slider-right')
-            .on('jcarouselcontrol:active', function() {
-                $(this).removeClass('inactive');
-            })
-            .on('jcarouselcontrol:inactive', function() {
-                $(this).addClass('inactive');
-            })
-            .jcarouselControl({
-                target: '+=1'
-            });
+// $(function(){
+//
+// //karu
+//
+//   $(".banners__set > .banners__link").on("click", function(e){
+//     e.preventDefault();
+//     if($(this).hasClass('active')){
+//       $(this).removeClass("active");
+//       $(this).siblings('.banners__content').slideUp(500);
+//       $(".banners__set > .banners__link i").removeClass("fa-minus").addClass("fa-plus");
+//     }else{
+//       $(".banners__set > .banners__link i").removeClass("fa-minus").addClass("fa-plus");
+//     $(this).find("i").removeClass("fa-plus").addClass("fa-minus");
+//     $(".banners__set > .banners__link").removeClass("active");
+//     $(this).addClass("active");
+//     $('.banners__content').slideUp(200);
+//     $(this).siblings('.banners__content').slideDown(200);
+//     }
+//     $('.fa-plus').text('+');
+//     $('.fa-minus').text('-');
+//   });
+// });;
