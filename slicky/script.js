03294b743eb7e7d1b271e222fa29bd097cc16ff9
ellert90 <yura.igm@gmail.com>
2018-01-18 00:18:54 +0200

    resize fix

--- slicky/script.js
+++ slicky/script.js
@@ -1,14 +1,26 @@
 var container = document.querySelectorAll('.container');
+var fixTitle = document.getElementById('head_title');
+var wrapper = document.getElementById('wrp');
+var titleElem = document.querySelector('.title');
+var titleStyle = getComputedStyle(titleElem);
+
+//щоб плавающий заголовок завжди був на всю ширину звичайного заголовка
+
+function resize () {
+  fixTitle.style.width = titleStyle.width;
+}
+
+window.onresize = resize();
 
 window.onscroll = function () {
-  var position = tes.getBoundingClientRect();
-  var fixTitle = head_title;
   var wrpScroll = fixTitle.getBoundingClientRect();
-  var wrpVar = wrp.getBoundingClientRect();
+  var wrpVar = wrapper.getBoundingClientRect();
 
+//прикріплює плаваючий заголовок до верху сторінки коли він виявляється у робочій зоні
   if (wrpVar.top <= 0) {
     fixTitle.style.position = 'fixed';
     fixTitle.style.visibility = 'visible';
+
   }
   if (wrpVar.bottom <= 0) {
     fixTitle.style.position = 'static';
@@ -19,6 +31,7 @@ window.onscroll = function () {
     fixTitle.style.visibility = 'hidden';
   }
 
+//перебирає блоки з текстом і заголовком, зчитує текст і колір потрібного заголовка і додає до плаваючого
   for (var i = 0; i < container.length; i++) {
     var cicle = container[i].getBoundingClientRect();
 
