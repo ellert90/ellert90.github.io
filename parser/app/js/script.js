5225cdf6de0c685c2a5de8bda099f8e6ba1da027
ellert90 <yura.igm@gmail.com>
2018-06-05 00:16:56 +0300

    change question in test

---
+++ parser/app/js/script.js
@@ -0,0 +1,10 @@
+var txt = document.querySelector('.wrapper');
+var test = "1. asdfsdfasdfasdf: dsafdsf. asefsd. 2."
+
+console.log(txt.typeOf);
+
+var a = /\d/;
+
+var res = test.match(a);
+
+console.log(res[0]);
