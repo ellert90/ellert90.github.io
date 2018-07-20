ce79377a8fdcc30c1699fcac4453c048451fe55c
ellert90 <yura.igm@gmail.com>
2018-07-20 22:33:44 +0300

    counter

---
+++ counter/app/js/script.js
@@ -0,0 +1,57 @@
+let obj = localStorage;
+let item = document.querySelector('.main');
+let items = document.querySelectorAll('.main__item_value');
+// console.log(item);
+
+  item.addEventListener('change', showAns);
+
+  function showAns(event) {
+    let target = event.target;
+    let index = event.target.getAttribute('data');
+    localStorage.setItem(index, target.value);
+    console.log(event.target.value);
+  }
+
+let left = 0,
+    leftIndex = 0;
+
+for(let i = 0; i < items.length; i++) {
+  let day = i + 1;
+  if (localStorage.getItem(day) != undefined) {
+    left += parseInt(localStorage.getItem(day));
+    leftIndex++;
+  }
+  // console.log(left);
+  items[i].value = localStorage.getItem(day);
+  // console.log(obj.n);
+}
+console.log(leftIndex);
+
+let summa = document.querySelector('#sum'),  //всього грошей
+    zal = document.querySelector('#zal'),   //скільки залишилося
+    serD = document.querySelector('#ser'),  //середні трати за день
+    zalS = document.querySelector('#zal_s'),  //не потрачене за день
+    norm = document.querySelector('#norm'); //норм трати на день
+
+norm.innerHTML = Math.floor(summa.textContent / 15);
+zal.innerHTML = summa.textContent - left;
+serD.innerHTML = Math.floor(left / leftIndex);
+zalS.innerHTML = Math.floor(summa.textContent / 15) - Math.floor(left / leftIndex);
+
+
+
+
+
+
+// onchange="localStorage.setItem('5', this.value)"
+// onchange="localStorage.setItem('6', this.value)"
+// onchange="localStorage.setItem('7', this.value)"
+// onchange="localStorage.setItem('8', this.value)"
+// onchange="localStorage.setItem('9', this.value)"
+// onchange="localStorage.setItem('0', this.value)"
+// onchange="localStorage.setItem('1', this.value)"
+// onchange="localStorage.setItem('2', this.value)"
+// onchange="localStorage.setItem('3', this.value)"
+// onchange="localStorage.setItem('4', this.value)"
+// onchange="localStorage.setItem('5', this.value)"
+// onchange="localStorage.setItem('6', this.value)"
