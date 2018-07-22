23da9a6747e624d413ec24567d7b0fdcf1f83774
ellert90 <yura.igm@gmail.com>
2018-07-22 15:58:22 +0300

    add calculator, add zalushok

--- counter/app/js/src/script.js
+++ counter/app/js/src/script.js
@@ -1,55 +1,53 @@
-let obj = localStorage;
-let item = document.querySelector('.main');
-let items = document.querySelectorAll('.main__item_value');
-// console.log(item);
+let obj = localStorage,
+    item = document.querySelector('.main'),
+    items = document.querySelectorAll('.main__item_value'),
+    left = 0,
+    leftIndex = 0,
+    summa = document.querySelector('#sum'),   //всього грошей
+    zal = document.querySelector('#zal'),     //скільки залишилося
+    serD = document.querySelector('#ser'),    //середні трати за день
+    zalS = document.querySelector('#zal_s'),  //залишок середнього
+    norm = document.querySelector('#norm'),   //норм трати на день
+    bonus = document.querySelector('#bon');   //скільки зекономлено
 
-  item.addEventListener('change', showAns);
+
+
+reDraw();
+item.addEventListener('change', showAns);
 
   function showAns(event) {
-    let target = event.target;
-    let index = event.target.getAttribute('data');
+    let target = event.target,
+        index = target.getAttribute('data'),
+        targetVal = parseInt(target.value),
+        localItem = parseInt(localStorage.getItem(index));
+
+    if (localItem && targetVal > 0) {
+      let newVal = parseInt(target.value);
+      let totalVal = localItem + newVal;
+      localStorage.setItem(index, totalVal);
+      reDraw();
+    }
     localStorage.setItem(index, target.value);
   }
 
-let left = 0,
-    leftIndex = 0;
 
-for(let i = 0; i < items.length; i++) {
-  let day = i + 1;
-  if (localStorage.getItem(day) != undefined) {
-    left += parseInt(localStorage.getItem(day));
-    leftIndex++;
+
+
+function reDraw () {
+  for(let i = 0; i < items.length; i++) {
+    let day = i + 1;
+    if (localStorage.getItem(day) != undefined) {
+      left += parseInt(localStorage.getItem(day));
+      leftIndex++;
+    }
+    items[i].value = localStorage.getItem(day);
   }
 
-  items[i].value = localStorage.getItem(day);
+  let zalSMath = Math.floor(summa.textContent / 15) - Math.floor(left / leftIndex);
 
+  norm.innerHTML = Math.floor(summa.textContent / 15);
+  zal.innerHTML = summa.textContent - left;
+  serD.innerHTML = Math.floor(left / leftIndex);
+  zalS.innerHTML = zalSMath;
+  bonus.innerHTML = zalSMath * leftIndex;
 }
-
-let summa = document.querySelector('#sum'),  //всього грошей
-    zal = document.querySelector('#zal'),   //скільки залишилося
-    serD = document.querySelector('#ser'),  //середні трати за день
-    zalS = document.querySelector('#zal_s'),  //не потрачене за день
-    norm = document.querySelector('#norm'); //норм трати на день
-
-norm.innerHTML = Math.floor(summa.textContent / 15);
-zal.innerHTML = summa.textContent - left;
-serD.innerHTML = Math.floor(left / leftIndex);
-zalS.innerHTML = Math.floor(summa.textContent / 15) - Math.floor(left / leftIndex);
-
-
-
-
-
-
-// onchange="localStorage.setItem('5', this.value)"
-// onchange="localStorage.setItem('6', this.value)"
-// onchange="localStorage.setItem('7', this.value)"
-// onchange="localStorage.setItem('8', this.value)"
-// onchange="localStorage.setItem('9', this.value)"
-// onchange="localStorage.setItem('0', this.value)"
-// onchange="localStorage.setItem('1', this.value)"
-// onchange="localStorage.setItem('2', this.value)"
-// onchange="localStorage.setItem('3', this.value)"
-// onchange="localStorage.setItem('4', this.value)"
-// onchange="localStorage.setItem('5', this.value)"
-// onchange="localStorage.setItem('6', this.value)"
